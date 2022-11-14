df9d73722675b32a16e88d4568b9446bc308439c
yao7san <yao7san@fake.com>
2022-11-14 02:52:20 +0000

    增加 CartSummary 的小部件，可以预览购物车内商品总价

---
+++ SportsStore/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Components
+{
+    public class CartSummaryViewComponent : ViewComponent
+    {
+        private Cart cart;
+
+        public CartSummaryViewComponent(Cart cart)
+        {
+            this.cart = cart;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
