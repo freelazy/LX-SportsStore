c14dbdd9445cb22f8fe175b08f2323a38e266c01
yao7san <yao7san@fake.com>
2022-11-15 11:19:10 +0800

    产品列表

---
+++ SportsStore/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models.Repository;
+
+namespace SportsStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        private IProductRepository repository;
+
+        public AdminController(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public IActionResult Index() => View(repository.Products);
+    }
+}
